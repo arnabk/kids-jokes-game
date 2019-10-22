662346035fbfede74896f5198e65738db3919508
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-10-22 00:44:08 +0000

    Deploy arnabk/kids-jokes-game to github.com/arnabk/kids-jokes-game.git:gh-pages

---
+++ precache-manifest.7edb74774b64df33d5878b033f888e0f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb4f4c9474550ab341d7035dda776661",
+    "url": "/kids-jokes-game/index.html"
+  },
+  {
+    "revision": "103d26012de30a1debef",
+    "url": "/kids-jokes-game/static/css/2.ff29e5f6.chunk.css"
+  },
+  {
+    "revision": "103d26012de30a1debef",
+    "url": "/kids-jokes-game/static/js/2.3b308f21.chunk.js"
+  },
+  {
+    "revision": "414fe1cdfe6fe9be4008",
+    "url": "/kids-jokes-game/static/js/main.607c7be8.chunk.js"
+  },
+  {
+    "revision": "dc91e916193cc90f4362",
+    "url": "/kids-jokes-game/static/js/runtime-main.79e4230a.js"
+  }
+]);
